95af97855ee0277778e9be44da94c19590a17c62
Johan Strootman <johanstr@users.noreply.github.com>
2025-01-17 09:12:03 +0100

    Aanvulling qua code van wat in les 01 af had moeten zijn

--- js/bke.js
+++ js/bke.js
@@ -49,16 +49,40 @@ const _CELL7 = 6;
 const _CELL8 = 7;
 const _CELL9 = 8;
 
-
-
-window.onload = function () {
+/*
+ * Met deze methode vertellen we de browser dat de functie die hier
+ * geprogrammeerd is gekoppeld moet worden aan de LOAD event.
+ * Deze code wordt dan automatisch uitgevoerd wanneer de browser
+ * klaar is met het verwerken en opslaan van de pagina in de DOM.
+ */
+window.onload = function() {
+   /*
+   * We gaan eerst alle elementen 'binnenhalen'
+   */
+   element_cells = document.querySelectorAll('.gameboard img');
+   element_game_button = document.querySelector('#game-button');
    element_turn_number = document.querySelector('.turn-player');
-   element_turn_player_image = document.querySelector('#img-turn-player');
+   element_turn_image = document.querySelector('#img-turn-player');
+   element_rounds_played = document.querySelector('.info-round p:last-child');
    element_score_player1 = document.querySelector('#score-player-one');
    element_score_player2 = document.querySelector('#score-player-two');
-   element_rounds_played = document.querySelector('.info-round p:last-child');
-   
-};
+   element_round_time = document.querySelector('.round-timer');
 
+   /*
+   * We gaan nu de hulp variabelen initialiseren met een startwaarde
+   * Voor b.v. scores geldt dat we de scores resetten naar 0,
+   * we beginnen dus opnieuw te tellen.
+   */
+   timer_id = null;            // NULL, want er is nog geen lopende timer
+   elapsedTimeInSeconds = 0;   // 0, want er is nog geen ronde gestart
+   score_player1 = 0;          // 0, reset van de score
+   score_player2 = 0;          // 0, reset van de score
+   current_round = 0;          // 0, want er is nog geen ronde gestart
+
+   // We gaan nu vertellen dat een click event op de button moet worden
+   // opgevangen en de afhandeling van een click door de functie
+   // buttonClick moet worden gedaan.
+   element_game_button.addEventListener('click', buttonClick);
+}
 
 
