66241612ea30f8969c1214e28d75b7ea7fc7545d
Johan Strootman <johanstr@users.noreply.github.com>
2025-01-24 10:25:38 +0100

    Voorbereiding op les 03 - Inspringen aangepast

--- js/bke.js
+++ js/bke.js
@@ -86,73 +86,72 @@ window.onload = function() {
 }
 
 function buttonClick(event_element) {
-    /* 
-        We controleren hier of de tekst op de button gelijk is aan: 'Start ronde'
-        Want als dat zo is dan moet er namelijk iets anders gebeuren dan wanneer
-        de tekst 'Reset ronde' op de button staat.
-     */
-    if(event_element.target.innerHTML == 'Start ronde') {
+   /* 
+      We controleren hier of de tekst op de button gelijk is aan: 'Start ronde'
+      Want als dat zo is dan moet er namelijk iets anders gebeuren dan wanneer
+      de tekst 'Reset ronde' op de button staat.
+   */
+   if(event_element.target.innerHTML == 'Start ronde') {
 
-        current_round++;                                        // Ronde nummer met 1 verhogen           
-        element_rounds_played.innerHTML = current_round;        // Nu plaatsen we het nummer in het element
-                                                                // om het te tonen aan de spelers
-        
-        // Willekeurig bepalen welke speler mag beginnen, dit levert een 1(speler 1) of een 2(speler 2) op
-        current_player = Math.floor(Math.random() * (2 - 1 + 1) + 1);  
-        
-        // Nu gaan we tonen op het scherm welke speler de ronde mag beginnen
-        element_turn_number.innerHTML = current_player;     // Het nummer tonen
-        element_turn_image.src = (current_player == _PLAYER1 ? _IMAGES[_PLAYER1] : _IMAGES[_PLAYER2]);
-        
-        /*
-            In de onderstaande programma lus lopen we nu langs alle cellen in het speelbord
-            en koppelen hier een click eventhandler aan.
-        */
-        element_cells.forEach(cell => {
-            cell.addEventListener('click', cellClick);
-        });
+      current_round++;                                        // Ronde nummer met 1 verhogen           
+      element_rounds_played.innerHTML = current_round;        // Nu plaatsen we het nummer in het element
+                                                               // om het te tonen aan de spelers
 
-        event_element.target.innerHTML = 'Reset ronde';     // Tekst op de knop veranderen
-        element_round_time.innerHTML = '00:00';             // We resetten en tonen een ronde tijd 00:00
-        
-        /*
-         * We starten nu de ronde timer
-         * Als echter de variabele timer_id niet gelijk is aan NULL dan loopt er nog 
-         * een timer, deze moeten we eerst stoppen.
-         */
-        if(timer_id != null) {          // Als de variabele timer_id niet NULL is is er al een timer
-            clearInterval(timer_id);    // Stop de timer
-            timer_id = null;            // Reset de variabele op NULL
-            elapsed_time_in_seconds = 0;   // Reset de variabele op 0 seconden
-        }
-        
-        timer_id = setInterval( roundsTimer, 1000 );    // Om de seconde mag de functie roundsTimer uitgevoerd worden
-    } else {
-        /*  Nee, dan gaan we het spel resetten door de volgende
-         *  stappen uit te voeren:
-         *      - Timer stoppen
-         *      - Laatste timer info tonen op het scherm
-         *      - Speelveld leeg maken (overal weer empty.jpg in plaatsen) en
-         *        Deactiveren van de click event handlers op de cellen
-         *      - Start spel op de button plaatsen
-         */
-        
-        /*  
-         *  We gaan nu eerst alle cellen in het speelveld onklikbaar maken
-         */
-        element_cells.forEach(cell => {
-            cell.removeEventListener('click', cellClick);
-            cell.src = _IMAGES[_EMPTY_CELL];
-        });
-        
-        event_element.target.innerHTML = 'Start ronde';     // Tekst op de knop veranderen
+      // Willekeurig bepalen welke speler mag beginnen, dit levert een 1(speler 1) of een 2(speler 2) op
+      current_player = Math.floor(Math.random() * (2 - 1 + 1) + 1);  
+   
+      // Nu gaan we tonen op het scherm welke speler de ronde mag beginnen
+      element_turn_number.innerHTML = current_player;     // Het nummer tonen
+      element_turn_image.src = (current_player == _PLAYER1 ? _IMAGES[_PLAYER1] : _IMAGES[_PLAYER2]);
+      
+      /*
+         In de onderstaande programma lus lopen we nu langs alle cellen in het speelbord
+         en koppelen hier een click eventhandler aan.
+      */
+      element_cells.forEach(cell => {
+         cell.addEventListener('click', cellClick);
+      });
 
-        // Nu stoppen we de ronde timer
-        if(timer_id != null) {          // Als de variabele timer_id niet NULL is is er al een timer
-            clearInterval(timer_id);    // Stop de timer
-            timer_id = null;            // Reset de variabele op NULL
-            elapsed_time_in_seconds = 0;   // Reset de variabele op 0 seconden
-        }
-    }
+      event_element.target.innerHTML = 'Reset ronde';     // Tekst op de knop veranderen
+      element_round_time.innerHTML = '00:00';             // We resetten en tonen een ronde tijd 00:00
+      
+      /*
+      * We starten nu de ronde timer
+      * Als echter de variabele timer_id niet gelijk is aan NULL dan loopt er nog 
+      * een timer, deze moeten we eerst stoppen.
+      */
+      if(timer_id != null) {          // Als de variabele timer_id niet NULL is is er al een timer
+         clearInterval(timer_id);    // Stop de timer
+         timer_id = null;            // Reset de variabele op NULL
+         elapsed_time_in_seconds = 0;   // Reset de variabele op 0 seconden
+      }
 
+      timer_id = setInterval( roundsTimer, 1000 );    // Om de seconde mag de functie roundsTimer uitgevoerd worden
+   } else {
+      /*  Nee, dan gaan we het spel resetten door de volgende
+       *  stappen uit te voeren:
+       *      - Timer stoppen
+       *      - Laatste timer info tonen op het scherm
+       *      - Speelveld leeg maken (overal weer empty.jpg in plaatsen) en
+       *        Deactiveren van de click event handlers op de cellen
+       *      - Start spel op de button plaatsen
+      */
+      
+      /*  
+       *  We gaan nu eerst alle cellen in het speelveld onklikbaar maken
+      */
+      element_cells.forEach(cell => {
+         cell.removeEventListener('click', cellClick);
+         cell.src = _IMAGES[_EMPTY_CELL];
+      });
+      
+      event_element.target.innerHTML = 'Start ronde';     // Tekst op de knop veranderen
+
+      // Nu stoppen we de ronde timer
+      if(timer_id != null) {          // Als de variabele timer_id niet NULL is is er al een timer
+         clearInterval(timer_id);    // Stop de timer
+         timer_id = null;            // Reset de variabele op NULL
+         elapsed_time_in_seconds = 0;   // Reset de variabele op 0 seconden
+      }
+   }
 }
